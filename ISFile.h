11817bf0a61218f0d8a93560e32aa0c1e6388980
kosskas <93159483+kosskas@users.noreply.github.com>
2023-11-24 17:36:44 +0100

    Add Record and Data structs

---
+++ ISFile.h
@@ -0,0 +1,14 @@
+#pragma once
+
+#include <iostream>
+#include <fstream>
+#include <string>
+#include "IBlock.h"
+#include "Index.h"
+#include "Record.h"
+using namespace std;
+
+class ISFile : public Block {
+
+};
+
---
+++ Record.h
@@ -0,0 +1,13 @@
+#pragma once
+struct Data {
+	int a, b, h;
+	Data() : a(0), b(0), h(0) {}
+	Data(int a, int b, int h) : a(a), b(b), h(h) {}
+};
+struct Record {
+	uint16_t key; //2
+	Data data; //12
+	uint16_t ofptr; //2
+	Record() : key(0), data({0,0,0}), ofptr(0) {}
+	Record(int key, Data data) : key(key), data(data), ofptr(0) {}
+};
