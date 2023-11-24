6389f5abc00548afcf33561ed0f904dd84c4fad7
kosskas <93159483+kosskas@users.noreply.github.com>
2023-11-24 19:07:05 +0100

    Add ptrs

--- Buffered.h
+++ Buffered.h
@@ -10,11 +10,15 @@ protected:
 	fstream* file;
 	string filename;
 	ios_base::openmode flags;
+	uint32_t w_ptr;
+	uint32_t r_ptr;
 
 public:
 	Buffered(string filename, ios_base::openmode flags) : filename(filename), flags(flags) {
 		file = new fstream();
 		file->open(filename, flags);
+		w_ptr= 0;
+		r_ptr = 0;
 	}
 	virtual void readBlock() = 0;
 	virtual void writeBlock(const char* block) = 0;
