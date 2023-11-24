09067b95b0d72c6939a276232a520be184f418b2
kosskas <93159483+kosskas@users.noreply.github.com>
2023-11-24 19:09:04 +0100

    Add readBlock in Index

--- Index.cpp
+++ Index.cpp
@@ -1,10 +1,17 @@
 #include "Index.h"
 
 Index::Index(int32_t BUFFSIZE, string filename, ios_base::openmode flags) {
+	this->BUFFSIZE = BUFFSIZE;
+	buffer = new IdxRec[BUFFSIZE];
 }
 
-void Index::readBlock()
-{
+void Index::readBlock() {
+	file->seekg(r_ptr);
+	uint32_t bytesRead = file->read((char*)buffer, sizeof(IdxRec)* BUFFSIZE).gcount();
+	if (bytesRead < sizeof(IdxRec) * BUFFSIZE) {
+		memset((char*)buffer + bytesRead, 0, sizeof(IdxRec) * BUFFSIZE - bytesRead); //jeœli przeczytano mniej ni¿ ca³¹ stronê, wyzeruj dalsze
+	}
+	r_ptr += bytesRead;
 }
 
 void Index::writeBlock(const char* block)
