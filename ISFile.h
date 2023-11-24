444bcd2b3e3b05be35dbd80e16d71a3b029353a6
kosskas <93159483+kosskas@users.noreply.github.com>
2023-11-24 17:43:10 +0100

    Add Index class

--- ISFile.h
+++ ISFile.h
@@ -8,7 +8,37 @@
 #include "Record.h"
 using namespace std;
 
-class ISFile : public Block {
 
+
+class ISFile : public Block {
+private:
+	fstream* file;
+	string filename;
+	ios_base::openmode flags;
+	uint32_t r_ptr;
+	uint32_t w_ptr;
+	uint32_t r_idx;
+	uint32_t w_idx;
+
+
+	Record buffer[BUFFSIZE] = { Record() };
+public:
+	void writeToBuff();
+	void writeToFile();
+
+	void forceWriteRecord(Record rec);
+
+	ISFile(string filename, ios_base::openmode flags);
+	Record readNext();
+
+	void writeRecord(Record rec);
+
+	void resetCursor();
+	void resetBufferPtr();
+	void clearBuffer();
+	void clearFile();
+	void printRecords();
+	void printBuffer();
+	~ISFile();
 };
 
--- main.cpp
+++ main.cpp
@@ -1,8 +1,35 @@
-#include <iostream>
+﻿#include <iostream>
 #include <string>
 #include <math.h>
 
 using namespace std;
+
+
+/*
+Oznaczenia:
+N – liczba rekordów w obszarze głównym pliku
+V - liczba rekordów w obszarze nadmiarowym pliku
+B – pojemność strony dyskowej (bajty)
+R – wielkość rekordu (bajty)
+K – wielkość klucza (bajty)
+P – wielkość wskaźnika (bajty)
+α – współczynnik wykorzystania strony w obszarze głównym po reorganizacji, <1
+
+Parametry:
+bf – współczynnik blokowania w obszarze głównym, równy floor( B / (R+P) )
+bi – współczynnik blokowania dla indeksu, równy floor( B / (K+P) )
+bi jest (R+P)/(K+P) razy większy niż bf (bo zwykle cały rekord jest dużo większy niż klucz)
+SN – wielkość obszaru głównego, w stronach, równa N/(bf × α)
+SOV – wielkość obszaru nadmiarowego, w stronach, równa V/bf (strony obszaru nadmiarowego są zapełniane do końca)
+SIN – wielkość indeksu 1. poziomu, w stronach, równa ceil( SN/bi )
+
+Zazwyczaj, bi >> 1 i SIN <<SN, zatem przeszukiwanie indeksu na dysku jest bardzo szybkie
+(lub indeks mieści się w pamięci operacyjnej i w ogóle nie są potrzebne dostępy do dysku).
+
+UWAGA: Indeksy w organizacji indeksowo-sekwencyjnej nazywamy indeksami rzadkimi (sparse):
+nie wszystkie rekordy są indeksowane, a jedynie tylko pierwsze na stronach obszaru głównego.
+
+*/
 
 int main(int argc, char** argv) {
 	return 0;
